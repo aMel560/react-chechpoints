3d24dcda40eb306a00e2d400f576407beedc0f12
aMel560 <83023664+aMel560@users.noreply.github.com>
2024-03-25 03:21:56 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import NavBar from "./components/navbar/navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import HoverCardDemo from "./components/card/card";
+function App() {
+  return (
+    <>
+      <NavBar />
+      <HoverCardDemo />
+    </>
+  );
+}
+
+export default App;
